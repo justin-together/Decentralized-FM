ba26d11b010eda05af407eee361c7d09fe77a86e
binhang <yuanbinhang@hotmail.com>
2022-08-23 18:37:46 +0200

    crusoe

--- dist_finetune_runner_w_euler_coordinator.py
+++ dist_finetune_runner_w_euler_coordinator.py
@@ -1,17 +1,15 @@
 import argparse
-import time
 import random
-import numpy as np
-import torch
+import os
 import torch.autograd.profiler as profiler
 from task_datasets.wikitext import get_wikitext_train_data_loader, get_wikitext_test_data_loader
 from task_datasets.wiki103 import get_wiki103_train_data_loader, get_wiki103_test_data_loader
 from task_datasets.arxiv21 import get_arxiv21_train_data_loader, get_arxiv21_test_data_loader
 from task_datasets.openwebtext import get_openwebtext_train_data_loader
-from modules.hf_gpt2_train_module import GPTConfig
+from task_datasets.fm_in_context_eval_data import get_fm_in_context_eval_train_data_loader
 from pipeline_parallel.dist_pp_utils import get_pp_finetune_module as get_pp_module
-
-from transformers import AutoTokenizer
+from coordinator.euler.coordinate_client import CoordinatorTrainClient
+from transformers import AutoTokenizer, AutoConfig
 
 try:
     import wandb
@@ -21,24 +19,78 @@ from utils.dist_args_utils import *
 from utils.dist_train_utils import *
 from utils.dist_test_utils import *
 from comm.comm_utils import *
-from coordinator.coordinate_client import *
+
+
+def save_checkpoint(args, pipe, ckpt_path):
+
+    use_dp = (args.world_size != args.pipeline_group_size)
+    if use_dp and get_data_parallel_rank() != 0:
+        return
+    
+    _layer_begin = get_pipeline_parallel_rank() * args.num_layers
+    _layer_end = min(_layer_begin + args.num_layers, args.max_layers)
+    
+    if get_pipeline_parallel_rank()  == 0:
+        torch.save(
+            pipe.model.model[0].state_dict(),
+            os.path.join(ckpt_path, f'pytorch_embs.pt')
+        )
+        
+        for i in range(_layer_begin, _layer_end):
+            print('saving layer', i)
+            torch.save(
+                pipe.model.model[i+1-_layer_begin].state_dict(),
+                os.path.join(ckpt_path, f'pytorch_{i}.pt')
+            )
+            
+    elif get_pipeline_parallel_rank()  == args.pipeline_group_size - 1:
+        for i in range(_layer_begin, _layer_end):
+            print('saving layer', i)
+            torch.save(
+                pipe.model.model[i-_layer_begin].state_dict(),
+                os.path.join(ckpt_path, f'pytorch_{i}.pt')
+            )
+        torch.save(
+            pipe.model.model[-1].state_dict(),
+            os.path.join(ckpt_path, f'pytorch_lm_head.pt')
+        )
+    else:
+        for i in range(_layer_begin, _layer_end):
+            print('saving layer', i)
+            torch.save(
+                pipe.model.model[i-_layer_begin].state_dict(),
+                os.path.join(ckpt_path, f'pytorch_{i}.pt')
+            )
 
 def train_loop(args, pipe, device, train_data_loader, test_data_loader):
     
+    print('tarting train loop....')
     for e in range(args.n_epochs):
         
+        print(f'==== epoch {e} ====')
+
         distributed_train_lm_iter(args, pipe, device, train_data_loader)
         
-        if test_data_loader is not None and args.do_evaluation:
+        if args.do_evaluation:
             distributed_test_lm_iter(args, pipe, device, test_data_loader)
             
         if get_pipeline_parallel_rank()  == args.pipeline_group_size - 1:
             if wandb is not None:
                 wandb.log({'epoch': e}, step=pipe.global_step)
+                
+        if args.model_save_path is not None:
+            ckpt_path = os.path.join(args.model_save_path, f'ckpt_epoch_{e}')
+            try:
+                os.makedirs(ckpt_path)
+            except Exception as e:
+                pass
+                
+            save_checkpoint(args, pipe, ckpt_path)
+            
 
 def main():
     parser = argparse.ArgumentParser(description='Gpipe-GPT3')
-    add_torch_distributed_w_coordinator_arguments(parser)
+    add_torch_distributed_w_euler_coordinator_arguments(parser)
     add_device_arguments(parser)
     # add_torch_distributed_arguments(parser)
     add_training_model_arguments(parser)
@@ -46,23 +98,7 @@ def main():
     add_training_hyper_parameter_arguments(parser)
     add_mixed_precision_arguments(parser)
     add_parallel_schema_arguments(parser)
-    parser.add_argument('--model-name', type=str, default='gpt2', metavar='S',
-                        help='model name or path')
-    parser.add_argument('--tokenizer-name', type=str, default='gpt2', metavar='S',
-                        help='tokenizer name or path')
-    parser.add_argument('--task-name', type=str, default='wikitext', metavar='S',
-                        help='task name')
-    parser.add_argument('--task-type', type=str, default='language_model', metavar='S',
-                        help='task typw')
-    parser.add_argument('--n-epochs', type=int, default=10, help='-')
-    parser.add_argument('--warmup-epochs', type=int, default=1, help='-')
-    parser.add_argument('--warmup-steps', type=int, default=None, help='-')
-    parser.add_argument('--total-steps', type=int, default=None, help='-')
-    parser.add_argument('--load-pretrained-model', 
-                        type=lambda x: x.lower()=='true', default=True, metavar='S',
-                        help='load pretrained model or not.')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
+    add_finetuning_model_arguments(parser)
     parser.add_argument('--profiling', type=str, default='tidy_profiling', metavar='S',
                         help='enable which profiling? default: tidy mode')
     parser.add_argument('--trace-postfix', type=str, default='default', metavar='S',
@@ -85,27 +121,10 @@ def main():
     args.rank = rank
     init_communicators_with_coordinator(args, prime_ip, rank)
     
-    config = GPTConfig.from_pretrained(args.model_name)
-#     config.attn_pdrop = 0.0
-#     config.embd_pdrop = 0.0
-#     config.resid_pdrop = 0.0
-#     config.summary_first_dropout = 0.0
-    
-    config.n_layer = args.num_layers
-#     if get_pipeline_parallel_rank() == args.pipeline_group_size-1:
-#         args.num_layers -= 3
-#         config.n_layer = args.num_layers  # num_layers per node
-#     elif get_pipeline_parallel_rank() == args.pipeline_group_size-4:
-#         args.num_layers += 1
-#         config.n_layer = args.num_layers  # num_layers per node
-#     elif get_pipeline_parallel_rank() == args.pipeline_group_size-3:
-#         args.num_layers += 1
-#         config.n_layer = args.num_layers  # num_layers per node
-#     elif get_pipeline_parallel_rank() == args.pipeline_group_size-2:
-#         args.num_layers += 1
-#         config.n_layer = args.num_layers  # num_layers per node
-#     else:
-#         config.n_layer = args.num_layers  # num_layers per node
+    config = AutoConfig.from_pretrained(args.model_name)
+    for k in config.__dict__:
+        if '_pdrop' in k or '_dropout' in k:
+            config.__dict__[k] = args.dropout
     
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
     tokenizer.model_max_length = args.seq_length
@@ -119,25 +138,31 @@ def main():
     random.seed(args.seed)
     np.random.seed(args.seed)
     
-    if args.task_name == 'wikitext':
-        train_data_loader = get_wikitext_train_data_loader(args, tokenizer)
-        test_data_loader = get_wikitext_test_data_loader(args, tokenizer)
-    elif args.task_name == 'wiki103':
-        train_data_loader = get_wiki103_train_data_loader(args, tokenizer)
-        test_data_loader = get_wiki103_test_data_loader(args, tokenizer)
-    elif args.task_name == 'arxiv21':
-        train_data_loader = get_arxiv21_train_data_loader(args, tokenizer)
-        test_data_loader = get_arxiv21_test_data_loader(args, tokenizer)
-    elif args.task_name == 'openwebtext':
-        train_data_loader = get_openwebtext_train_data_loader(args, tokenizer)
-        test_data_loader = get_wikitext_test_data_loader(args, tokenizer)
+    if get_pipeline_parallel_rank() == 0:
+        if args.task_name == 'wikitext':
+            train_data_loader = get_wikitext_train_data_loader(args, tokenizer)
+            test_data_loader = get_wikitext_test_data_loader(args, tokenizer)
+        elif args.task_name == 'wiki103':
+            train_data_loader = get_wiki103_train_data_loader(args, tokenizer)
+            test_data_loader = get_wiki103_test_data_loader(args, tokenizer)
+        elif args.task_name == 'arxiv21':
+            train_data_loader = get_arxiv21_train_data_loader(args, tokenizer)
+            test_data_loader = get_arxiv21_test_data_loader(args, tokenizer)
+        elif args.task_name == 'openwebtext':
+            train_data_loader = get_openwebtext_train_data_loader(args, tokenizer)
+            test_data_loader = get_wikitext_test_data_loader(args, tokenizer)
+        elif args.task_name == 'fm_in_context_eval':
+            train_data_loader = get_fm_in_context_eval_train_data_loader(args, tokenizer)
+            test_data_loader = None
+        else:
+            raise Exception('unknown task.')
     else:
-        raise Exception('unknown task.')
-        
+        train_data_loader, test_data_loader = None, None
+            
     if args.warmup_steps is None:
-        args.warmup_steps = len(train_data_loader)
+        args.warmup_steps = 0 #len(train_data_loader)
     if args.total_steps is None:
-        args.total_steps = len(train_data_loader) * args.n_epochs
+        args.total_steps = 0 #len(train_data_loader) * args.n_epochs
     
     use_dp = (args.world_size != args.pipeline_group_size)
     if use_dp:
@@ -151,35 +176,6 @@ def main():
     
     print('initializing pipeline')
     pipe = get_pp_module(args, config, device, use_dp)
-    
-    if args.load_pretrained_model:
-        print('loading model')
-        if get_pipeline_parallel_rank() == 0:
-            pipe.model.model[0].load_state_dict(
-                torch.load(f'{args.model_name}/pytorch_embs.pt')
-            )
-            for i in range(len(pipe.model.model)-1):
-                print(i)
-                pipe.model.model[i+1].load_state_dict(
-                    torch.load(f'{args.model_name}/pytorch_{i}.pt')
-                )
-        elif get_pipeline_parallel_rank() == args.pipeline_group_size-1:
-            _i = get_pipeline_parallel_rank() * args.num_layers
-            for i in range(len(pipe.model.model)-1):
-                print(_i + i)
-                pipe.model.model[i].load_state_dict(
-                    torch.load(f'{args.model_name}/pytorch_{_i + i}.pt')
-                )
-            pipe.model.model[-1].load_state_dict(
-                torch.load(f'{args.model_name}/pytorch_lm_head.pt')
-            )
-        else:
-            _i = get_pipeline_parallel_rank() * args.num_layers
-            for i in range(len(pipe.model.model)):
-                print(_i + i)
-                pipe.model.model[i].load_state_dict(
-                    torch.load(f'{args.model_name}/pytorch_{_i + i}.pt')
-                )      
 
     print('starting train loop....')
     if args.profiling == 'no-profiling':
@@ -195,6 +191,7 @@ def main():
             try:
                 train_loop(args, pipe, device, train_data_loader, test_data_loader)
             except Exception as e:
+                raise e
                 print(get_pipeline_parallel_rank(), e)
             pipe.export_profiling_result(filename=trace_file)
         elif args.profiling == 'pytorch_profiling':
@@ -210,4 +207,9 @@ def main():
     coord_client.notify_train_finish(message=train_finish_msg)
 
 if __name__ == '__main__':
+
+    import datasets
+    # euler distributed file system makes cache slow
+    datasets.disable_caching()
+
     main()
--- dist_inference_server_w_euler_coordinator.py
+++ dist_inference_server_w_euler_coordinator.py
@@ -4,15 +4,12 @@ from pipeline_parallel.dist_pp_utils import get_pp_inference_module
 from utils.dist_args_utils import *
 from utils.dist_inference_utils import *
 from comm.comm_utils import *
-from coordinator.coordinate_client import *
-from transformers import AutoTokenizer
-from task_datasets.inference_data import get_request_processor
-
+from coordinator.euler.coordinate_client import CoordinatorInferenceClient
 
 def main():
     parser = argparse.ArgumentParser(description='Inference Runner with coordinator.')
     add_device_arguments(parser)
-    add_torch_distributed_inference_w_coordinator_arguments(parser)
+    add_torch_distributed_inference_w_euler_coordinator_arguments(parser)
     add_inference_arguments(parser)
     add_inference_details_arguments(parser)
     parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -36,30 +33,20 @@ def main():
 
     init_inference_communicators_with_coordinator(args, prime_ip, rank, port=port)
 
-    if get_pipeline_parallel_rank() == 0 or True:
-
-        request_processor = get_request_processor(args)
-        request_processor.set_arguments(args)
-
-    else:
-        tokenizer = None
-        request_processor = None
-        print('warning: todo: arguments specified in the request will not take effect.')
-
     pipe = get_pp_inference_module(args, device, rank=rank)
 
     if args.profiling == 'no-profiling':
-        avg_iter_time = distributed_inference_mask_iter(args, pipe, device, request_processor)
+        avg_iter_time = distributed_inference_mask_server(args, pipe, device)
     else:
         prefix = './trace_json/inference_' + args.pp_mode
         trace_file = prefix + get_inference_arguments_str(args, rank=rank) + '_' + args.profiling + '_' + args.trace_postfix + \
                      '.json'
         if args.profiling == 'tidy_profiling':
-            avg_iter_time = distributed_inference_mask_iter(args, pipe, device, request_processor)
+            avg_iter_time = distributed_inference_mask_server(args, pipe, device)
             pipe.export_profiling_result(filename=trace_file)
         elif args.profiling == 'pytorch_profiling':
             with profiler.profile(profile_memory=True, use_cuda=args.use_cuda) as prof:
-                avg_iter_time = distributed_inference_mask_iter(args, pipe, device, request_processor)
+                avg_iter_time = distributed_inference_mask_server(args, pipe, device)
             print(prof.key_averages().table())
             prof.export_chrome_trace(trace_file)
         else:
